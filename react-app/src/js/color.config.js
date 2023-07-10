963f746ba35463086fd76c33b07949083f7e34c5
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2023-07-10 16:13:03 -0600

    ColorConfig

--- react-app/src/js/color.config.js
+++ react-app/src/js/color.config.js
@@ -1,22 +1,29 @@
 import {useColorMode} from '@docusaurus/theme-common';
+import {useEffect} from 'react';
 
 export default function SafariColor(){
 
   const {colorMode} = useColorMode();
+  const colorpalette = ['#023047','#ef233c','#0095b6','#8d99ae']
+  const col = colorpalette[Math.floor(Math.random() * colorpalette.length)]
 
-  //const metaThemeColor = document.querySelector('meta[name=theme-color]');
-  //metaThemeColor.setAttribute('content', colorMode === 'dark' ? '#000000' : '#A41034');
+  useEffect(() => {
+
+  const metaThemeColor = document.querySelector('meta[name=theme-color]');
+  metaThemeColor.setAttribute('content', colorMode === 'dark' ? '#000000' : col);
 
   const head = document.getElementsByTagName('head')[0];
 
   let s;
 
-  const selection_css = '::selection { background: #A41034' + '; color: #fff; }'
+  const selection_css = '::selection { background: #ef233c' + '; color: #fff; }'
   s = document.createElement('style');
   s.setAttribute('type', 'text/css');
   s.appendChild(document.createTextNode(selection_css));
   head.appendChild(s);
 
   return null;
+
+  }, []);
   
 }
